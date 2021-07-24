e380a0242cad988307b64891ccb02bba42416fe2
Abbosjon(U1910298) <nosirovabosjon3429@gmail.com>
2021-07-24 16:53:29 +0500

    tailwindcss was added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h1>This is App component</h1>
+      <h1 className="text-red-600 bg-yellow-200 font-bold tracking-wider">This is App component</h1>
     </div>
   );
 }
